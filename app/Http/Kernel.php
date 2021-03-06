263979148b8a5d8c2753b0a630202aa3dd1b715c
Sanjay Prajapati <sanjay@moweb.com>
2021-03-06 15:29:29 +0530

    make preventbackhistory middleware in laravel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'checkPermission' => \App\Http\Middleware\CheckPermission::class,
         'cors' => \App\Http\Middleware\CorsMiddleware::class,
         'routeRestriction' => \App\Http\Middleware\RouteRestriction::class,
+        'preventBackHistory' => \App\Http\Middleware\PreventBackHistory::class,
     ];
 }
