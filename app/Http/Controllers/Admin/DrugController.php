4626bd66602908bd31e96f5078e61a2918471cfd
Sanjay Prajapati <sanjay14prajapati1998@gmail.com>
2021-03-18 00:45:04 +0530

    make drug type and drug unit dynamic

--- app/Http/Controllers/Admin/DrugController.php
+++ app/Http/Controllers/Admin/DrugController.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Str;
 use DataTables;
 use App\User;
 use App\Drug;
+use App\DrugType;
+use App\DrugUnit;
 use Exception;
 use Image;
 use Auth;
@@ -40,10 +42,12 @@ class DrugController extends BaseController
             'user' => Auth::user(),
         ];
         if ($request->ajax()) {
-            $drugs = Drug::where('added_by', [Auth::id()])->orderBy('id', 'DESC')->get();
+            $drugs = Drug::with('drugUnit')->where('added_by', [Auth::id()])->orderBy('id', 'DESC')->get();
             return Datatables::of($drugs)
-                ->addColumn('strength', function ($data) {
-                    return ($data->unit == 'other') ? $data->strength . $data->other_unit : $data->strength . $data->unit;
+                ->addColumn('type', function ($data) {
+                    return $data->drugType->name;
+                })->addColumn('strength', function ($data) {
+                    return ($data->unit == 'other') ? $data->strength . $data->other_unit : $data->strength . $data->drugUnit->name;
                 })->addColumn('instructions', function ($data) {
                     return '<span class="ws-break-spaces">' . $data->instructions . '</span>';
                 })->addColumn('action', function ($row) {
@@ -65,8 +69,8 @@ class DrugController extends BaseController
     public function create()
     {
         $data = ['title' => 'Add Drug'];
-        $data['type'] = config('view.Drug_Type');
-        $data['unit'] = config('view.Dosage_Unit');
+        $data['type'] = DrugType::orderBy('name', 'asc')->pluck('name', 'id');
+        $data['unit'] = DrugUnit::orderBy('name', 'asc')->pluck('name', 'id');
         $html = view('admin.drug.create', $data)->render();
         $result = ['status' => $this->success, 'message' => 'load drug data.', 'html' => $html];
 
@@ -126,8 +130,8 @@ class DrugController extends BaseController
     {
         $data['title'] = 'Edit drug';
         $data['drug'] = Drug::find($id);
-        $data['type'] = config('view.Drug_Type');
-        $data['unit'] = config('view.Dosage_Unit');
+        $data['type'] = DrugType::orderBy('name', 'asc')->pluck('name', 'id');
+        $data['unit'] = DrugUnit::orderBy('name', 'asc')->pluck('name', 'id');
         $html = view('admin.drug.update', $data)->render();
         $result = ['status' => $this->success, 'message' => 'load drug data.', 'html' => $html];
         return Response::json($result);
