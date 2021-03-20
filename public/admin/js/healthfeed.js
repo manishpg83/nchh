6cf21db686bf06eb379913a3e5a2d6d73d1b2ed1
Sanjay Prajapati <sanjay14prajapati1998@gmail.com>
2021-03-20 15:07:23 +0530

    add validation on content field when store healthfeed

--- public/admin/js/healthfeed.js
+++ public/admin/js/healthfeed.js
@@ -107,9 +107,18 @@ function init_healthfeed_form() {
             },
 			content: "required"
         },
+        ignore: ".note-editor *",
         messages: {
             category_ids: "Please select health feed category",
-            title: "Please enter health feed title"
+            title: "Please enter health feed title",
+            content: 'Please enter health feed content'
+        },
+        errorPlacement: function(error, element) {
+            if(element.hasClass("summernote")) {
+              error.insertAfter('.note-editor');
+            } else {
+              error.insertAfter(element);
+            }
         },
         submitHandler: function(form) {
             var action = $(form).attr("action");
@@ -154,7 +163,14 @@ function init_healthfeed_form() {
     $(".summernote").summernote({
         dialogsInBody: true,
         minHeight: 200,
-        maxHeight: 250
+        maxHeight: 250,
+        callbacks: {
+            onChange: function(contents, $editable) {
+                $(".summernote").val($(".summernote").summernote('isEmpty') ? $(".note-editable").html('') : contents);
+
+                $("#healthfeedForm").valid();
+            }
+        }
     });
 }
 
